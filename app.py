1b953132ac02c089d04fa5593a14bfb54365c459
Gandharv Kulkarni <114131432+gandharvk422@users.noreply.github.com>
2025-08-12 19:19:40 +0530

    FlickSift

---
+++ app.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import pickle as pkl
+
+st.set_page_config(page_title="FlickSift - Discover Your Perfect Movie Instantly", page_icon=":movie:")
+
+with open("similarity.pkl", "rb") as file:
+    similarity = pkl.load(file)
+
+with open("movies_list.pkl", "rb") as file:
+    movies = pkl.load(file)
+
+# movies = pkl.load(open("movies_list.pkl", "rb"))
+# similarity = pkl.load(open("similarity.pkl", "rb"))
+movies_list = movies["title"].values
+
+st.title("FlickSift")
+st.header("Discover Your Perfect Movie Instantly")
+
+selectvalue = st.selectbox("Select movie from dropdown", movies_list)
+
+def recommend(movie):
+    index = movies[movies["title"] == movie].index[0]
+    distance = sorted(list(enumerate(similarity[index])), reverse=True, key = lambda vector:vector[1])
+    
+    recommend_movie = []
+
+    for i in distance[1:6]:
+        recommend_movie.append(movies.iloc[i[0]].title)
+    return recommend_movie
+
+if st.button("Show Recommendations"):
+    movie_name = recommend(selectvalue)
+    col1, col2, col3, col4, col5 = st.columns(5)
+    st.text(movie_name[0])
+    st.text(movie_name[1])
+    st.text(movie_name[2])
+    st.text(movie_name[3])
+    st.text(movie_name[4])
